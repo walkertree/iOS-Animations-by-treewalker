96ef6451e38b9f038e86a0a41f48d13c961615f1
蜗壳 <treewalker.fu@gmail.com>
2016-03-15 15:41:11 +0800

    springs 动画

--- README.md
+++ README.md
@@ -2,7 +2,25 @@
 ##简单的视图动画
 
 ```
-+ (void)animateWithDuration:(NSTimeInterval)duration animations:(void (^)(void))animations 
++ (void)animateWithDuration:(NSTimeInterval)duration 
+					animations:(void (^)(void))animations 
 
 ```
-改方法可以使用的 UIView 的属性 ：bounds、frame、center、backgroundColor、alpha
+改方法可以使用的 UIView 的属性 ：bounds、frame、center、backgroundColor、alpha、transform
+
+###springs
+```
++ (void)animateWithDuration:(NSTimeInterval)duration 
+							delay:(NSTimeInterval)delay 
+ 		usingSpringWithDamping:(CGFloat)dampingRatio 
+ 		initialSpringVelocity:(CGFloat)velocity 
+ 					options:(UIViewAnimationOptions)options
+				animations:(void (^)(void))animations 
+				completion:(void (^ __nullable)(BOOL finished))completion NS_AVAILABLE_IOS(7_0);
+
+```
+具有弹性的动画：
+
+usingSpringWithDamping ： 代表弹的强度，0.0 - 1.0，数值越小，表示弹的范围越大。
+
+initialSpringVelocity：表示动画的初始速度，数值越大，初始速度越大，初始速度大，也可以造成弹簧来回的速度显示。
--- iOS-Animations-by-treewalker/iOS-Animations-by-treewalker/ViewController.m
+++ iOS-Animations-by-treewalker/iOS-Animations-by-treewalker/ViewController.m
@@ -18,6 +18,7 @@
 @property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
 @property (weak, nonatomic) IBOutlet UIButton *loginButton;
 @property (weak, nonatomic) IBOutlet UILabel *headLabel;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *spinerView;
 
 @end
 
@@ -27,6 +28,8 @@
     [super viewDidLoad];
     
     [self.passwordTextField addObserver:self forKeyPath:@"center" options:NSKeyValueObservingOptionNew context:nil];
+    
+    
 }
 
 
@@ -37,18 +40,24 @@
     self.topRightImage.alpha = 0.0f;
     self.bottomLeftImage.alpha = 0.0f;
     self.bottomRightImage.alpha = 0.0f;
-
-}
-
-- (void)viewDidLayoutSubviews
-{
-    [super viewDidLayoutSubviews];
+    
     self.headLabel.center = CGPointMake(self.headLabel.center.x - self.view.frame.size.width,
                                         self.headLabel.center.y) ;
     self.passwordTextField.center = CGPointMake(self.passwordTextField.center.x - self.view.bounds.size.width,
                                                 self.passwordTextField.center.y) ;
     self.nameTextField.center = CGPointMake(self.nameTextField.center.x - self.view.bounds.size.width,
                                             self.nameTextField.center.y) ;
+    
+    self.loginButton.center = CGPointMake(self.loginButton.center.x,
+                                          self.view.center.y + 100);
+    self.loginButton.alpha = 0.0f;
+    self.spinerView.alpha = 0.0f;
+}
+
+- (void)viewDidLayoutSubviews
+{
+    [super viewDidLayoutSubviews];
+  
 }
 
 
@@ -68,7 +77,7 @@
     } completion:nil];
     [UIView animateKeyframesWithDuration:1.0f
                                    delay:0.7f
-                                 options:UIViewKeyframeAnimationOptionRepeat|UIViewKeyframeAnimationOptionAutoreverse|UIViewAnimationOptionCurveEaseInOut /*有加速减速显示的过程*/
+                                 options:UIViewKeyframeAnimationOptionCalculationModeLinear
                               animations:^{
         self.passwordTextField.center = CGPointMake(self.view.center.x,
                                                 self.passwordTextField.center.y) ;
@@ -77,9 +86,9 @@
     
     
     
-    [UIView animateKeyframesWithDuration:5.0f
-                                   delay:4.0f
-                                 options:UIViewKeyframeAnimationOptionRepeat                              animations:^{
+    [UIView animateKeyframesWithDuration:2.0f
+                                   delay:1.0f
+                                 options:UIViewKeyframeAnimationOptionCalculationModeLinear                              animations:^{
                                   self.topLeftImage.alpha = 1.0f;
                                   self.topRightImage.alpha = 1.0f;
                                   self.bottomLeftImage.alpha = 1.0f;
@@ -87,11 +96,61 @@
                                   
                               } completion:nil];
 
+    [UIView animateWithDuration:0.5
+                          delay:0.5
+         usingSpringWithDamping:0.5
+          initialSpringVelocity:0
+                        options:UIViewAnimationOptionTransitionNone
+                     animations:^{
+                         self.loginButton.center = CGPointMake(self.loginButton.center.x,
+                                                              self.view.center.y );
+                         self.loginButton.alpha = 1.0f;
+                    
+                     }
+                     completion:nil];
+
 }
 
 -(void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSString *,id> *)change context:(void *)context
 {
     NSLog(@"%@",keyPath);
+}
+
+- (IBAction)loginPress:(id)sender {
+    
+    [UIView animateWithDuration:1.5
+                          delay:0.0
+         usingSpringWithDamping:0.2
+          initialSpringVelocity:0
+                        options:UIViewAnimationOptionTransitionNone
+                     animations:^{
+                         self.loginButton.bounds = CGRectMake(self.loginButton.bounds.origin.x,
+                                                             self.loginButton.bounds.origin.y,
+                                                             self.loginButton.bounds.size.width + 30 ,
+                                                             self.loginButton.bounds.size.height);
+                     } completion:nil];
+    [UIView animateWithDuration:0.33
+                          delay:0.0
+         usingSpringWithDamping:0.7
+          initialSpringVelocity:0.0
+                        options:UIViewAnimationOptionTransitionNone
+                     animations:^{
+                         self.loginButton.center = CGPointMake(self.loginButton.center.x,
+                                                               self.loginButton.center.y + 30 );
+                         self.loginButton.backgroundColor = [UIColor colorWithRed:0.84 green:0.83 blue:0.62 alpha:1.0f];
+                         
+                         self.spinerView.center = CGPointMake(self.loginButton.frame.origin.x + 20,
+                                                              self.loginButton.frame.origin.y + self.loginButton.bounds.size.height / 2);
+                        self.spinerView.alpha = 1.0;
+                         
+                         self.passwordTextField.frame = CGRectMake(self.passwordTextField.frame.origin.x, self.passwordTextField.frame.origin.y, self.passwordTextField.frame.size.width + 30, self.passwordTextField.frame.size.height);
+                     }
+                     completion:nil];
+    
+
+
+
+    [self.spinerView startAnimating];
 }
 
 
