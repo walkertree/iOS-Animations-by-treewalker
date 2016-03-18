dd979dcee9d2e15b30f4467d891b88e5e8b0b027
蜗壳 <treewalker.fu@gmail.com>
2016-03-18 15:25:10 +0800

    Auto Layout

--- iOS-Animations-by-treewalker/iOS-Animations-by-treewalker/ViewController.m
+++ iOS-Animations-by-treewalker/iOS-Animations-by-treewalker/ViewController.m
@@ -8,7 +8,19 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+#define Items (@[@"Icecream money", @"Great weather", @"Beach ball", @"Swim suit for him", @"Swim suit for her", @"Beach games", @"Ironing board", @"Cocktail mood", @"Sunglasses", @"Flip flops"])
+
+@interface ViewController () <UITableViewDataSource,UITableViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@property (weak, nonatomic) IBOutlet UILabel *labelTitle;
+
+@property (weak, nonatomic) IBOutlet UIButton *buttonMenu;
+
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *menuHeightConstraint;
+
+@property (nonatomic, assign) BOOL isMenuOpen;
 
 @end
 
@@ -16,12 +28,90 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.isMenuOpen = NO;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+}
+- (IBAction)menuButtonPress:(id)sender {
+    
+    self.isMenuOpen = !self.isMenuOpen;
+    self.menuHeightConstraint.constant = self.isMenuOpen == YES ? 200 : 60;
+    self.labelTitle.text = self.isMenuOpen ? @"Select Item" : @"Packing List";
+    self.buttonMenu.transform = self.isMenuOpen ?CGAffineTransformMakeRotation(M_PI_4) : CGAffineTransformIdentity;
+    
+    for (NSLayoutConstraint *item in self.labelTitle.superview.constraints)
+    {
+        if(item.secondItem == self.labelTitle && item.secondAttribute == NSLayoutAttributeCenterX)
+        {
+            item.constant = self.isMenuOpen ? 100:0;
+            NSLog(@"%@,%d,%d",item,item.secondAttribute,item.firstAttribute);
+            continue;
+
+        }
+        
+        if(item.firstItem == self.labelTitle && item.firstAttribute == NSLayoutAttributeCenterY)
+        {
+            [self.labelTitle.superview removeConstraint:item];
+            continue;
+
+        }
+        
+        NSLayoutConstraint *newConstraint = [NSLayoutConstraint constraintWithItem:self.labelTitle
+                                                                         attribute:NSLayoutAttributeCenterY
+                                                                         relatedBy:NSLayoutRelationEqual
+                                                                            toItem:self.labelTitle.superview
+                                                                         attribute:NSLayoutAttributeCenterY
+                                                                        multiplier:(self.isMenuOpen ? 0.67:1.0)
+                                                                          constant:5.0];
+        newConstraint.active = YES;
+        
+    }
+    
+    
+    
+    [UIView animateWithDuration:1.0
+                          delay:0.0
+         usingSpringWithDamping:0.4
+          initialSpringVelocity:10
+                        options:UIViewAnimationOptionCurveEaseIn
+                     animations:^{
+                         //这一句有加，才会有动画
+                         [self.view layoutIfNeeded];
+                     } completion:^(BOOL finished) {
+                     }];
+}
+
+- (void) showItem:(NSInteger) index
+{
+    NSLog(@"tapped item : %@",Items[index]);
+}
+
+
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return Items.count;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    cell.accessoryType = UITableViewCellAccessoryNone;
+    cell.textLabel.text = Items[indexPath.row];
+    cell.imageView.image = [UIImage imageNamed:[NSString stringWithFormat:@"summericons_100px_0%d.png",indexPath.row ]];
+    return cell;
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    [self showItem:indexPath.row];
 }
 
 @end
